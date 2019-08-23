0f1d0b485d93108b8696e926396098632ed68862
iaine <iain.emsley@kellogg.ox.ac.uk>
2019-08-23 14:48:49 +0100

    Initial Haskell TimeModel

---
+++ model.hs
@@ -0,0 +1,15 @@
+module Models
+( TimeModel
+)
+
+-- |The TimeModel is based on Yuk Hui's definitions in his book on Digital Objects
+data TimeModel = TimeModel { mechanical :: Float,
+    clock :: Float,  
+    topological :: Int
+    } deriving (Show)
+
+getOffset :: TimeModel -> Int -> Int
+getOffset tm start = topological tm - start
+
+getTimes :: [TimeModel] -> [(Int, Float)]
+[TimeModel] ts = [(mechanical t, topological t - start) | t <- ts ]
