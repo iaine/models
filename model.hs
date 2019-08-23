2ae9c6a16d4949b26bc317006a28750326692de2
iaine <iain.emsley@kellogg.ox.ac.uk>
2019-08-23 14:50:29 +0100

    Updated with the offset time

--- model.hs
+++ model.hs
@@ -12,4 +12,7 @@ getOffset :: TimeModel -> Int -> Int
 getOffset tm start = topological tm - start
 
 getTimes :: [TimeModel] -> [(Int, Float)]
-[TimeModel] ts = [(mechanical t, topological t - start) | t <- ts ]
+[TimeModel] ts = [(mechanical t, topological t) | t <- ts ]
+
+getTimesOffset :: [TimeModel] -> [(Int, Float)]
+[TimeModel] ts = [(mechanical t, topological t, topological t - start) | t <- ts ]
