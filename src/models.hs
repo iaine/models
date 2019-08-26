a316818c67a7e93da1f4ad38be2ef34455c667b9
iaine <iain.emsley@kellogg.ox.ac.uk>
2019-08-26 21:35:40 +0100

    Fix to models and main

--- src/models.hs
+++ src/models.hs
@@ -1,10 +1,12 @@
 -- models.hs
-module Models where
+module Main where
 
-import TimeModels
+import TimeModel
 
-main = let t = TimeModel 1.0 1.0 1345
-       let t1 = TimeModel 2.0 2.0 1456
-       let t2 = TimeModel 3.0 3.0 1673
-       let ts = [t, t1, t2]
-       getOffset ts
+main = do let t = TimeModel 1.0 1.0 1345
+          let t1 = TimeModel 2.0 2.0 1456
+          let t2 = TimeModel 3.0 3.0 1673
+          let ts = [t, t1, t2]
+          print(getTimes ts)
+          let y = [ topological t3 - 1300 | t3 <- ts]
+          print (y)
