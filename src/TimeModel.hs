50f5914d0894fa0595ee2e9eddddf8b8cc0aa0cb
iaine <iain.emsley@kellogg.ox.ac.uk>
2019-08-26 21:36:18 +0100

    Updated functions

--- src/TimeModel.hs
+++ src/TimeModel.hs
@@ -1,6 +1,4 @@
-module Models
-( TimeModel
-)
+module TimeModel where
 
 -- |The TimeModel is based on Yuk Hui's definitions in his book on Digital Objects
 data TimeModel = TimeModel { mechanical :: Float,
@@ -11,8 +9,5 @@ data TimeModel = TimeModel { mechanical :: Float,
 getOffset :: TimeModel -> Int -> Int
 getOffset tm start = topological tm - start
 
-getTimes :: [TimeModel] -> [(Int, Float)]
-[TimeModel] ts = [(mechanical t, topological t) | t <- ts ]
-
-getTimesOffset :: [TimeModel] -> [(Int, Float)]
-[TimeModel] ts = [(mechanical t, topological t, topological t - start) | t <- ts ]
+getTimes :: [TimeModel] -> [(Float, Float, Int)]
+getTimes tms = [(mechanical t, clock t, topological t) | t <- tms ]
