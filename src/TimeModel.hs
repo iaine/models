97c85940322590cc4301b4100c0821dca6a58664
iaine <iain.emsley@kellogg.ox.ac.uk>
2019-09-27 21:59:38 +0100

    Add the new mahcine times

--- src/TimeModel.hs
+++ src/TimeModel.hs
@@ -2,12 +2,21 @@ module TimeModel where
 
 -- |The TimeModel is based on Yuk Hui's definitions in his book on Digital Objects
 data TimeModel = TimeModel { mechanical :: Float,
-    clock :: Float,  
+    clock :: Int,  
     topological :: Int
     } deriving (Show)
 
 getOffset :: TimeModel -> Int -> Int
 getOffset tm start = topological tm - start
 
-getTimes :: [TimeModel] -> [(Float, Float, Int)]
+getTimes :: [TimeModel] -> [(Float, Int, Int)]
 getTimes tms = [(mechanical t, clock t, topological t) | t <- tms ]
+
+createTimes :: [Int] -> [(TimeModel)]
+createTimes is = [TimeModel 1.0 k i | i <- is, let k=head [1..3]]
+
+createTimesOffset :: [Int] -> [(TimeModel)]
+createTimesOffset is = [TimeModel 1.0 k j | i <- is, let k=head [1..3], let j = i-1300]
+
+alterTime ::  [Int]  -> [Int]
+alterTime as = [y | a <- as, let y = round(fromIntegral a / 1000000)]
--- src/models.hs
+++ src/models.hs
@@ -3,10 +3,23 @@ module Main where
 
 import TimeModel
 
-main = do let t = TimeModel 1.0 1.0 1345
-          let t1 = TimeModel 2.0 2.0 1456
-          let t2 = TimeModel 3.0 3.0 1673
+main = do let t = TimeModel 1.0 1 1345
+          let t1 = TimeModel 2.0 2 1456
+          let t2 = TimeModel 3.0 3 1673
           let ts = [t, t1, t2]
           print(getTimes ts)
           let y = [ topological t3 - 1300 | t3 <- ts]
           print (y)
+          let times = [1341, 1675, 1642, 1588]
+          print(times)
+          let times1 = createTimes times
+          print (getTimes times1)
+          let times2 = createTimesOffset times
+          print (getTimes times2)
+          let otherClock = [15848430000, 12994900000, 1694945660000]
+          print ("Machine times")
+          print (otherClock)
+          let alterOther = alterTime otherClock
+          print (alterOther)
+          let times3 = createTimes alterOther
+          print (times3)
